7ad3d2b41e89f0b3f8349b2ba223b68b7b8b05b5
Filipe Soares <filipecarlossoares@gmail.com>
2021-01-05 20:11:15 -0300

    GET /products

---
+++ backend/src/main/java/com/simpledevelopment/dsdeliver/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.simpledevelopment.dsdeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simpledevelopment.dsdeliver.dto.ProductDTO;
+import com.simpledevelopment.dsdeliver.services.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+
+	@GetMapping
+	public ResponseEntity<List<ProductDTO>> findAll() {
+		List<ProductDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
