25f52382499131709b2350550385b4261c8f2bf5
Filipe Soares <filipecarlossoares@gmail.com>
2021-01-09 13:14:20 -0300

    Add product list

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,16 +1,25 @@
-import './styles.css'
-import StepsHeader from './StepsHeader';
+import { useEffect, useState } from "react";
+import SetpsHeader from "./StepsHeader";
+import "./styles.css";
+import ProductsList from "./ProductsList";
+import { Product } from "./types";
+import { fetchProducts } from "../api";
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then((response) => setProducts(response.data))
+            .catch((error) => console.log(error));
+    }, []);
 
     return (
-
         <div className="orders-container">
-            <StepsHeader />
+            <SetpsHeader />
+            <ProductsList products={products} />
         </div>
-
-    )
-
+    );
 }
 
 export default Orders;
